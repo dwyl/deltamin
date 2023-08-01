36a4bfe61b5994475ed6424ecefcd7fee9a13d7f
Luís Arteiro <lucho14garfield@gmail.com>
2023-08-01 14:36:33 +0100

    feat: Initial commit.

---
+++ lib/deltamin.dart
@@ -0,0 +1,11 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+class Awesome {
+  bool get isAwesome => true;
+}
+
+
+// TODO: Export any libraries intended for clients of this package.
